7188785f5a592ac27e8214598b8abbad5b8c64b4
Marcin <m.metera91@gmail.com>
2025-03-24 10:15:47 +0100

    add test_main file, add pytest in requirments and modify main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 def factorials(n):
+    if n < 0 or n != int(n):
+        raise ValueError
+
+
     factorial = 1
-    for i in range(1, n + 1):
-        factorial *= i
+    if n == 0:
         yield factorial
+    else:
+        for i in range(1, n + 1):
+            factorial *= i
+            yield factorial
 
 
 def main():
---
+++ test_main.py
@@ -0,0 +1,12 @@
+import pytest
+from main import factorials
+
+
+def test_factorial_zero():
+    result = list(factorials(0))
+    assert result == [1]
+
+
+def test_factorial_positive():
+    result = list(factorials(5))
+    assert result == [1, 2, 6, 24, 120]
